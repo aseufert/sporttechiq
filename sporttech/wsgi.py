c6ce3127a9e88b1540355299d3e9d99bf40632b6
Alex Seufert <amseufert@gmail.com>
2018-12-26 12:55:36 -0500

    fixed wsgi

--- sporttech/wsgi.py
+++ sporttech/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 from django.core.wsgi import get_wsgi_application
 import os
 
-with open(site_config) as f:
-    configs = json.loads(f.read())
-    os.environ["DJANGO_SETTINGS_MODULE"] = configs['SETTINGS']
+os.environ["DJANGO_SETTINGS_MODULE"] = "sporttech.settings.staging"
 
 application = get_wsgi_application()
