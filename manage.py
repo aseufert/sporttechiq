8cb41ae8bc1670d3420b3a76821c3dcd25fe9f90
Alex Seufert <amseufert@gmail.com>
2018-12-26 17:30:29 -0500

    fixed bug related to django config in manage.py file

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+import json
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sporttech.settings.staging")
+    PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    site_config = os.path.join(PROJECT_DIR, 'sporttech', 'sporttech', 'settings', 'site_config.json')
+
+    with open(site_config) as f:
+        configs = json.loads(f.read())
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = configs['SETTINGS']
 
     from django.core.management import execute_from_command_line
 
