bbd36bea35869cc83db0b6f75de71ddcb9123c89
Alex Seufert <amseufert@gmail.com>
2018-12-26 12:34:28 -0500

    changed wsgi settings

--- sporttech/wsgi.py
+++ sporttech/wsgi.py
@@ -8,5 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 from django.core.wsgi import get_wsgi_application
+import os
+
+with open(site_config) as f:
+    configs = json.loads(f.read())
+    os.environ["DJANGO_SETTINGS_MODULE"] = configs['SETTINGS']
 
 application = get_wsgi_application()
