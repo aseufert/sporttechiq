81c0019ae90e3b08a824f4f20cb75522980dec6b
Alex Seufert <amseufert@gmail.com>
2019-01-26 12:30:09 -0500

    linter fixes

--- manage.py
+++ manage.py
@@ -3,12 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    import os
     import json
     from django.core.management import execute_from_command_line
 
     FILE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    site_config = os.path.join(FILE_DIR, 'sporttech','sporttech', 'settings', 'site_config.json')
+    site_config = os.path.join(FILE_DIR, 'sporttech', 'sporttech', 'settings', 'site_config.json')
 
     with open(site_config) as f:
         configs = json.loads(f.read())
